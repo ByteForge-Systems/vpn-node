72239e58ea4697c7feb92b4a29bc8bdb3c6fb10a
Artem Efremov <artem.efremov.art@outlook.com>
2025-02-23 10:45:34 +0300

    Feature/new endpoints (#2)
    * микро фикс обработчика для добавляения пользователя + добавления эндпоинта для получения всех пользователей
    
    * описание эндпоинта для списка всех юзеров
    
    * фикс обработчика для добавления юзера + новый эндпоинт
    
    * фикс
    
    * изменение readme

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -5,18 +5,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/ByteForge-Systems/vpn-node/scripts"
 )
-
 // Обработчик для эндпоинтов пользователей
 
 func AddUser(c *gin.Context) {
-	var user struct {
-		ID string `json:"id"`
-	}
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
 	userID, err := scripts.GenerateUser()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -24,6 +15,16 @@ func AddUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"id": userID})
+}
+
+func ListAllUsers(c *gin.Context) {
+    users, err := scripts.ListUsers()
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"users": users})
 }
 
 func RemoveUser(c *gin.Context) {
--- api/routes/user_routes.go
+++ api/routes/user_routes.go
@@ -10,6 +10,7 @@ func SetupUserRoutes(router *gin.Engine) {
 	{
 		user.POST("/", handlers.AddUser)
 		user.DELETE("/:id", handlers.RemoveUser)
+		user.POST("/get/all", handlers.ListAllUsers)
 		user.GET("/:id/link", handlers.GenerateVLESSLink)
 	}
 }
